2717cfa35658be761fde9e1ca21e8b98a747485e
Daryu-Kim <dev_daryu@kakao.com>
2022-12-30 15:20:05 +0900

    2022-12-30 15:20 Update

--- js/index.js
+++ js/index.js
@@ -2,4 +2,20 @@ let topBtn = document.querySelector(".content-pageup");
 
 topBtn.onclick = () => window.scrollTo({top:0, behavior: "smooth" });
 window.onscroll = () => window.scrollY > 500? topBtn.getElementsByClassName.opacity = 1 :
-topBtn.getElementsByClassName.opacity = 0
+topBtn.getElementsByClassName.opacity = 0;
+
+
+let header_nav_btn = document.getElementById("header-nav-btn");
+let header_nav = document.getElementById("header-nav");
+var mcnt = 0;
+
+header_nav_btn.click(function(){
+    if (mcnt == 0){     
+        header_nav.addClass('trans');
+        mcnt = 1;
+        return;
+    } else {
+        header_nav.removeClass('trans');
+        mcnt = 0;
+    }
+});
